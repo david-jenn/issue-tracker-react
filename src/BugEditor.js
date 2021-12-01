ebc5254a07384db412a5207c1d3be0f4dd427270
David Jenn <david_jenn@insideranken.org>
2021-12-01 16:15:40 -0600

    fixes

--- src/BugEditor.js
+++ src/BugEditor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useEffect } from 'react/cjs/react.development';
+
 import { moment } from 'moment';
 import _ from 'lodash';
 
