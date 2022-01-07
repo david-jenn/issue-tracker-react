cdb4c92969aae19d956cfe1d839dea2cea7df5c0
David Jenn <david_jenn@insideranken.org>
2022-01-07 16:13:17 -0600

    react pwa

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import 'bootstrap'
-import './index.css'
-
-
+import 'bootstrap';
+import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,11 +11,27 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// check if browser supports service workers
+if (navigator.serviceWorker) {
+  // wait for the page to load
+  window.addEventListener('load', () => {
+    if (process.env.NODE_ENV === 'production') {
+      // register service worker in prod environment
+      navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`);
+    } else {
+      // unregister service worker in dev environment
+      navigator.serviceWorker.ready
+        .then((registration) => registration.unregister())
+        .catch((error) => console.error(error.message));
+    }
+  });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
